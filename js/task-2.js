7b1efc0cc0b09c9fbf6105e5d2389726dc28ac4f
Tetiana Proshyna <mollinne2@gmail.com>
2020-12-23 21:53:10 +0200

    Add 1-10 tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,9 @@
+//Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+import users from './users.js';
 
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter(user => user.eyeColor === color);
+};
+
+console.log(getUsersWithEyeColor(users, 'blue'));
+// [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,9 @@
+//Получить массив только неактивных пользователей (поле isActive).
+import users from './users.js';
 
+const getInactiveUsers = users => {
+  return users.filter(user => !user.isActive);
+};
+
+console.log(getInactiveUsers(users));
+// [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
